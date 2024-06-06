e18f33d7feb67353c6610cd75895f1ab7074db5e
Lopimaterkus <julien.baraer29@gmail.com>
2024-06-06 19:25:36 +0200

    Changement h1 banner + css

--- my-app/src/components/Banner/index.jsx
+++ my-app/src/components/Banner/index.jsx
@@ -4,7 +4,7 @@ export default function Banner({ image, title }) {
   return (
     <div className="banner-container">
       <img className="banner-img" src={image} alt="Banner" />
-      <h1 className="banner-title">{title}</h1>
+      {title && <h1 className="banner-title">{title}</h1>}
     </div>
   )
 }
